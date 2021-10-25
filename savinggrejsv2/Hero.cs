d9e5f13ec8f43227b2d5f09cd8d997235eb3728b
christos <potamitischristos@gmail.com>
2021-10-25 09:17:01 +0200

    Initial commit

---
+++ savinggrejsv2/Hero.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace savinggrejsv2
+{
+    public class Hero
+    {
+        public string name { get; set; }
+        public int hp { get; set; }
+        public int damage { get; set; }
+
+
+    }
+}
---
+++ savinggrejsv2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace savinggrejsv2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string svar;
+
+            Hero player = new Hero();
+
+            System.Console.WriteLine("Vad vill din hero heta?");
+
+            svar = Console.ReadLine();
+
+            player.name = svar;
+
+            System.Console.WriteLine("Hur mycket skada vill du göra?");
+
+            svar = Console.ReadLine();
+
+            player.damage = int.Parse(svar);
+
+            System.Console.WriteLine("Hur mycket hp vill du ha?");
+
+            svar = Console.ReadLine();
+
+            player.hp = int.Parse(svar);
+
+
+
+            string json;
+
+            json = JsonSerializer.Serialize<Hero>(player);
+
+            File.WriteAllText(@"text.json", json);
+
+
+
+
+        }
+    }
+}
